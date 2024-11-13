f3341007dc2e6b4ec9aa1204ef82827da4240691
xyue24 <xyue24@bu.edu>
2024-11-13 18:32:28 -0500

    v0.8
    Platform Deploy Ready

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -9,8 +9,16 @@ firebase_admin.initialize_app(cred)
 
 database = firestore.client()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../Frontend', template_folder='../Frontend')
 CORS(app)
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.template_folder, 'MainPage.html')
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
 
 #   Frontend APIs
 # route for file uploads
